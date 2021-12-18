65b2041119e42d4082842048d56db925560756f1
Yonghwi Kim <yh0921k@gmail.com>
2021-12-18 21:55:08 +0900

    add: 03-01 기본타입 변수

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,13 @@
+// JS 문자열 선언
+// var hello = "hello";
+
+// TS 문자열 선언
+const hello = 'hello';
+
+// TS 숫자 선언
+const num = 10;
+
+// TS 배열 선언
+const arr: Array<number> = [1, 2, 3];
+const heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+const items: number[] = [1, 2, 3];
