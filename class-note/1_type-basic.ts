915609ea0e590eac3e383ed15818e53dc0b35b5b
Yonghwi Kim <yh0921k@gmail.com>
2021-12-18 22:08:32 +0900

    add: 03-02 기본타입 변수2

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -11,3 +11,17 @@ const num = 10;
 const arr: Array<number> = [1, 2, 3];
 const heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
 const items: number[] = [1, 2, 3];
+
+// TS 튜플
+const address: [string, number] = ['gangnam', 100];
+
+// TS 객체
+const obj: object = {};
+const person: object = {
+  name: 'capt',
+  age: 100,
+};
+const thor: { name: string; age: number } = { name: 'Thor', age: 1000 };
+
+// TS 진위값
+const show = true;
