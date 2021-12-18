7e3766c227376380086a226b856e0bdf8b089f3e
Yonghwi Kim <yh0921k@gmail.com>
2021-12-19 00:46:26 +0900

    add: 04-05 실습풀이(Filter API & 화살표 함수 소개)

--- quiz/1_todo/index.ts
+++ quiz/1_todo/index.ts
@@ -30,11 +30,11 @@ function completeTodo(index: number, todo: object): void {
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo(): object {
   return todoItems[0];
 }
 
-function showCompleted() {
+function showCompleted(): object[] {
   return todoItems.filter(item => item.done);
 }
 
@@ -44,7 +44,7 @@ function addTwoTodoItems() {
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
