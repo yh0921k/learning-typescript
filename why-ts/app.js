318d538ba87ccba46ce32555ba84b6655f9736ab
Yonghwi Kim <yh0921k@gmail.com>
2021-12-18 18:32:21 +0900

    add: 01-03 자바스크립트에 타입이 있을 때의 첫 번째 장점

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,6 +1,6 @@
 // api url
-// var url = "https://jsonplaceholder.typicode.com/users/1";
-var url = "https://jsonplaceholder.typicode.com/users";
+var url = "https://jsonplaceholder.typicode.com/users/1";
+// var url = "https://jsonplaceholder.typicode.com/users";
 
 // dom
 var username = document.querySelector("#username");
@@ -10,22 +10,43 @@ var address = document.querySelector("#address");
 // user data
 var user = {};
 
-function startApp() {
-    axios
-        .get(url)
-        .then(function (response) {
-            // console.log(response);
-            user = response.data;
+/**
+ * @typedef {object} address
+ * @property {string} street
+ * @property {string} city
+ */
 
-            // TODO: 이름, 이메일, 주소 표시하기
-            console.log(user)
-            username.innerText = user[0].name;
-            email.innerText = user[0].email;
-            address.innerText = user[0].address.street;
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+function startApp() {
+  // axios
+  //     .get(url)
+  fetchUser()
+    .then(function (response) {
+      // console.log(response);
+      user = response.data;
+
+      // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+      username.innerText = user.name;
+      email.innerText = user.email;
+      address.innerText = user.address.street;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
 startApp();
