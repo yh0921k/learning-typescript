482b7d64ddeb6c21b54e8ffd8e21177901dc2e09
Yonghwi Kim <yh0921k@gmail.com>
2021-12-19 11:05:15 +0900

    add: 07-01 연산자를 이용한 타입 정의(Union Type)

---
+++ class-note/5_operator.ts
@@ -0,0 +1,15 @@
+// function logMessage(value: any) {
+//   console.log(value);
+// }
+//
+// logMessage('hello');
+// logMessage(199);
+// logMessage(false);
+
+function logMessage(value: string | number | boolean) {
+  console.log(value);
+}
+
+logMessage('hello');
+logMessage(199);
+logMessage(false);
