2b916d875ff9caebc20dfde65e0e38cbe86372e9
Yonghwi Kim <yh0921k@gmail.com>
2021-12-19 23:32:48 +0900

    add: 12-02 인터페이스와 제네릭을 이용한 타입 추론 방식

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -1,6 +1,18 @@
+// 타입 추론 기본
 let a = 10;
 
 function getB(b = 10) {
   let c = 'abc';
   return b + c;
 }
+
+// 타입 추론 기본 2
+interface Dropdown<T> {
+  value: T;
+  title: string;
+}
+
+let shoppingItem: Dropdown<string> = {
+  value: 'abc',
+  title: 'hello',
+};
