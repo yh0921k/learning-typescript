30e62fcec0a0070400f4517280bbc92856f1998d
Yonghwi Kim <yh0921k@gmail.com>
2021-12-19 11:21:46 +0900

    add: 07-02 유니온 타입의 장점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -6,10 +6,15 @@
 // logMessage(199);
 // logMessage(false);
 
-function logMessage(value: string | number | boolean) {
-  console.log(value);
+function logMessage(value: string | number) {
+  if (typeof value === 'number') {
+    value.toLocaleString();
+  }
+  if (typeof value === 'string') {
+    value.toString();
+  }
+  throw new TypeError('value must be string or number');
 }
 
 logMessage('hello');
 logMessage(199);
-logMessage(false);
