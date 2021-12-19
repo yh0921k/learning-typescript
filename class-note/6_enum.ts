a2c5ef2fca3641f68cceb9b03cf4261a3d860295
Yonghwi Kim <yh0921k@gmail.com>
2021-12-19 12:18:19 +0900

    add: 08-04 이넘 활용 사례

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -5,3 +5,21 @@ enum Shoes {
 
 let myShoes = Shoes.Nike;
 console.log(myShoes); // 'nike'
+
+// 예제
+enum Answer {
+  Yes = 'Y',
+  No = 'N',
+}
+function askQuestion(answer: Answer) {
+  if (answer === Answer.Yes) {
+    console.log('correct');
+  }
+  if (answer === Answer.No) {
+    console.log('incorrect');
+  }
+}
+askQuestion(Answer.Yes);
+// askQuestion('yes');
+// askQuestion('예스');
+// askQuestion('y');
