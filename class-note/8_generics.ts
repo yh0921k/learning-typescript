2ab59f379fa60067649a2add636d0bf1375af9da
Yonghwi Kim <yh0921k@gmail.com>
2021-12-19 14:06:53 +0900

    add: 10-03 기존 타입 정의 방식과 제네릭의 차이점(함수 중복 선언의 단점)

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -6,10 +6,24 @@
 // logText('Hi');
 // logText(true);
 
-function logText<T>(text: T): T {
+// function logText<T>(text: T): T {
+//   console.log(text);
+//   return text;
+// }
+// logText<number>(10);
+// logText('Hi');
+// logText(true);
+
+function logText(text: string) {
   console.log(text);
+  // text.split('').reverse().join('');
   return text;
 }
-logText<number>(10);
-logText('Hi');
-logText(true);
+
+function logNumber(num: number) {
+  console.log(num);
+  return num;
+}
+
+logText('a');
+const num = logNumber(10);
