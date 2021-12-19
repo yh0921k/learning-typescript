a9b788898f034ec0ed887e465774f4b5293128f6
Yonghwi Kim <yh0921k@gmail.com>
2021-12-20 02:13:08 +0900

    add: 13-02 타입 단언 예제

--- class-note/10_type-assertion.ts
+++ class-note/10_type-assertion.ts
@@ -3,3 +3,14 @@ let a;
 a = 20;
 a = 'a';
 let b = a as string;
+
+// DOM API 조작
+// <div id="app">hi</div>
+
+// 아래의 단언(as)로 인해 null과 union 하지 않음
+// 즉, 단언은 개발자가 TS보다 타입에 대해 더 잘 알고 있는 경우 책임을 위임
+let div = document.querySelector('div') as HTMLDivElement;
+let app = document.querySelector('#app');
+if (div) {
+  div.innerText = 'aaa';
+}
