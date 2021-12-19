13f11b55184a81f6a73307421786d19a5385947a
Yonghwi Kim <yh0921k@gmail.com>
2021-12-20 02:56:40 +0900

    add: 16-04 전하번호부 어플리케이션에 모듈화 실습

---
+++ quiz/2_address-book/types.ts
@@ -0,0 +1,19 @@
+export interface PhoneNumberDictionary {
+  [phone: string]: {
+    num: number;
+  };
+}
+
+export interface Contact {
+  name: string;
+  address: string;
+  phones: PhoneNumberDictionary;
+}
+
+export enum PhoneType {
+  Home = 'home',
+  Office = 'office',
+  Studio = 'studio',
+}
+
+// export { Contact, PhoneType };
