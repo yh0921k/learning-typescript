238b40f00b3444bc4863e3776e3c56f8c3d46855
Yonghwi Kim <yh0921k@gmail.com>
2021-12-19 12:35:23 +0900

    add: 09-03 자바스크립트 프로토타입 활용 사례

--- class-note/class.js
+++ class-note/class.js
@@ -7,23 +7,27 @@ class Person {
   }
 }
 
-let capt = new Person('capt', 100);
-console.log(capt);
+// let capt = new Person('capt', 100);
+// console.log(capt);
 
 // Prototype
 
 // var user = { name: 'capt', age: 100 };
 // var admin = { name: 'capt', age: 100, role: 'admin' };
 
-var user = { name: 'capt', age: 100 };
-var admin = {};
-admin.__proto__ = user;
-console.log(admin.__proto__);
-console.log(admin.__proto__.__proto__);
-console.log(admin.name);
-console.log(admin.age);
+// var user = { name: 'capt', age: 100 };
+// var admin = {};
+// admin.__proto__ = user;
+// console.log(admin.__proto__);
+// console.log(admin.__proto__.__proto__);
+// console.log(admin.name);
+// console.log(admin.age);
+//
+// admin.role = 'admin';
+// console.log(admin.name);
+// console.log(admin.age);
+// console.log(admin.role);
 
-admin.role = 'admin';
-console.log(admin.name);
-console.log(admin.age);
-console.log(admin.role);
+// Built-in API, Native Javascript API
+var obj = { a: 10 };
+console.log(obj); // 브라우저 개발자 도구 참조 및 MDN Object 참조
