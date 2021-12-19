b1f23b1c59e137907956a5bd3573d1ecc5df0ce1
Yonghwi Kim <yh0921k@gmail.com>
2021-12-19 23:38:13 +0900

    add: 12-03 복잡한 구조에서의 타입 추론 방식

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -7,12 +7,30 @@ function getB(b = 10) {
 }
 
 // 타입 추론 기본 2
+// interface Dropdown<T> {
+//   value: T;
+//   title: string;
+// }
+//
+// let shoppingItem: Dropdown<string> = {
+//   value: 'abc',
+//   title: 'hello',
+// };
+
+// 타입 추론 기본 3
 interface Dropdown<T> {
   value: T;
   title: string;
 }
 
-let shoppingItem: Dropdown<string> = {
-  value: 'abc',
-  title: 'hello',
+interface DetailedDropdown<K> extends Dropdown<K> {
+  description: string;
+  tag: K;
+}
+
+let detailedItem: DetailedDropdown<string> = {
+  title: 'abc',
+  description: 'ab',
+  value: 'val',
+  tag: 'tag',
 };
