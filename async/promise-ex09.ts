6a38713470cfa5afe2230e6bab1fec0d52a7af84
Yonghwi Kim <yh0921k@gmail.com>
2022-01-10 15:40:44 +0900

    add: Promise 예제 09

---
+++ async/promise-ex09.ts
@@ -0,0 +1,18 @@
+function requestDataFirst(): Promise<any> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(10);
+    }, 1000);
+  });
+}
+
+function requestDataSecond(): Promise<any> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(20);
+    }, 1000);
+  });
+}
+
+requestDataFirst().then(data => console.log(data));
+requestDataSecond().then(data => console.log(data));
